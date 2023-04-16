2055a77b5eeee843eb8fc547de2d4814fcf95ea4
Gabrel Messias Costa <gmessiasc@gmail.com>
2023-04-16 16:48:18 -0300

    feat - exercise 2: impar ou par

--- M2S02/exercises/solutions/exercises/Exercise 2.cs
+++ M2S02/exercises/solutions/exercises/Exercise 2.cs
@@ -3,9 +3,30 @@ namespace ConsoleExercise
     
     class Exercise2 : ExerciseBase
     {
+        private int number;
+
+        private void verifyNumber() {
+    
+            if (!int.TryParse(Console.ReadLine(), out this.number)) {
+                Console.Write("\t Por favor, insira um número inteiro e válido: ");
+                verifyNumber();
+                return;
+            };
+
+            Console.Write($"\n \t O número {this.number} é um número");
+
+            if (this.number % 2 != 0) {
+                Console.Write(" impar. \n");
+            } else {
+                Console.Write(" par. \n");
+            }
+        }
         public override void execute()
         {
-            Console.WriteLine("Executando exercicio 2...");
+            Console.Clear();
+            Console.WriteLine("\t Executando exercicio 2!");
+            Console.Write("\n \t Por favor, insira um número e descobriremos se ele é par ou impar: ");
+            this.verifyNumber();
         }
     }
 }
