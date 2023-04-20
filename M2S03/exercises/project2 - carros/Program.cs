d7bae99cc183e91237d8dee428cfbf38aefdcaa9
Gabriel Messias Costa <gmessiasc@gmail.com>
2023-04-20 20:59:39 -0300

    feat - some classes

--- M2S03/exercises/project2 - carros/Program.cs
+++ M2S03/exercises/project2 - carros/Program.cs
@@ -13,4 +13,22 @@ Owner isabela = new Owner(  "Isabela Luciana Pinto",
                             "(11) 98729-3196");
 
 
-Car car1 = new Car()
+Car car1 = new Car( "Buggy",
+                    "Buggy 1.6/ TST/ RS 1.6 4-Lug.",
+                    "LVU-4557",
+                    "Bege",
+                    nicolas);
+
+Car car2 = new Car("JPX",
+                    "Jipe Montez 4x4 CD Teto R.",
+                    "HRR-8147",
+                    "Bege",
+                    cecilia);
+
+Car car3 = new Car("CHANGAN",
+                    "MINI STAR Utility 1.0 8V 53cv (Furg",
+                    "ANU-2758",
+                    "Cinza",
+                    isabela);
+
+Console.Write(car1);
